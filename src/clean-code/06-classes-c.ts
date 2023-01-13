9fc27357f65339e306c9ad435f12a9fe131c472b
Matias Hamie <mhamie@perceptyx.com>
2023-01-12 22:49:22 -0300

    principio de composicion agregado

---
+++ src/clean-code/06-classes-c.ts
@@ -0,0 +1,99 @@
+(() => {
+  // Aplicando el principio de responsabilidad única
+  // Priorizar la composicion frente a la herencia
+  type Gender = "M" | "F";
+
+  interface PersonProps {
+    birthdate: Date;
+    gender: Gender;
+    name: string;
+  }
+
+  class Person {
+    public birthdate: Date;
+    public gender: Gender;
+    public name: string;
+
+    constructor({ name, gender, birthdate }: PersonProps) {
+      this.name = name;
+      this.gender = gender;
+      this.birthdate = birthdate;
+    }
+  }
+
+  interface UserProps {
+    email: string;
+    role: string;
+  }
+
+  class User {
+    public email: string;
+    public lastAccess: Date;
+    public role: string;
+    constructor({ email, role }: UserProps) {
+      this.email = email;
+      this.lastAccess = new Date();
+      this.role = role;
+    }
+
+    checkCredentials() {
+      return true;
+    }
+  }
+
+  interface SettingsProps {
+    workingDirectory: string;
+    lastOpenFolder: string;
+  }
+
+  class Settings {
+    public workingDirectory: string;
+    public lastOpenFolder: string;
+    constructor({ workingDirectory, lastOpenFolder }: SettingsProps) {
+      this.lastOpenFolder = lastOpenFolder;
+      this.workingDirectory = workingDirectory;
+    }
+  }
+
+  interface UserSettingsProps {
+    workingDirectory: string;
+    lastOpenFolder: string;
+    email: string;
+    role: string;
+    name: string;
+    gender: Gender;
+    birthdate: Date;
+  }
+
+  class UserSettings {
+    public person: Person;
+    public user: User;
+    public setttings: Settings;
+
+    constructor({
+      name,
+      gender,
+      birthdate,
+      email,
+      role,
+      lastOpenFolder,
+      workingDirectory,
+    }: UserSettingsProps) {
+      this.person = new Person({ name, gender, birthdate });
+      this.user = new User({ email, role });
+      this.setttings = new Settings({ lastOpenFolder, workingDirectory });
+    }
+  }
+
+  const userSettings = new UserSettings({
+    workingDirectory: "usr/home",
+    lastOpenFolder: "/home",
+    email: "email@email.com",
+    role: "admin",
+    name: "Matias",
+    gender: "M",
+    birthdate: new Date("1990-10-08"),
+  });
+
+  console.log(userSettings);
+})();
